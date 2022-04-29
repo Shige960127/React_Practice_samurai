b0c7825d09d8aa4b812475e3ef709cd5a530535a
Shige960127 <yudake231@icloud.com>
2022-04-29 18:18:40 +0900

    ログインボタンの作成

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
 import React, { useState } from "react";
-// import "./style.css"
 
-const InputCheckBox = () => {
-const [checkedValues, setCheckedValues] = useState([]);
-const handleChange = (e) => {
-  if (checkedValues.includes(e.target.value)) {
-    setCheckedValues(
-      checkedValues.filter((checkedValues) =>
-        checkedValues !== e.target.value)
-    )
-  } else {
-    setCheckedValues([...checkedValues, e.target.value]);
+
+const LogoutButton = (props) => (
+  <button onClick={props.toggleIsLoggedIn}>Logout</button>
+)
+const LoginButton = (props) => (
+  <button onClick={props.toggleIsLoggedIn}>Login</button>
+)
+
+const LoginControl = () => {
+  const [loginState, setLoginState] = useState(false);
+  const toggleIsLoggedIn = () => {
+    setLoginState(!loginState);
   }
-};
-
-return (
-  <div className="App">
-    <p>
-      <b>{checkedValues.join("、")}</b>
-    </p>
-    <label>
-      <input type="checkbox" value="マウス"
-        onChange={handleChange}
-        checked={checkedValues.includes("マウス")} />
-    </label>
-    <label>
-      <input type="checkbox" value="モニター"
-        onChange={handleChange}
-        checked={checkedValues.includes("モニター")} />
-    </label>
-    <label>
-      <input type="checkbox" value="キーボード"
-        onChange={handleChange}
-        checked={checkedValues.includes("キーボード")} />
-    </label>
-  </div>
-);
+  if (loginState) {
+    return < LogoutButton toggleIsLoggedIn={toggleIsLoggedIn} />
+  }
+  return <LoginButton toggleIsLoggedIn={toggleIsLoggedIn} />
 };
 
 export default function App() {
-  return <inputCheckBox />
-} 
+  return <LoginControl />
+}
